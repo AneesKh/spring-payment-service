20774ed7dd1d13b00266c009daea675beaf3cf3e
cp <k.anees93@gmail.com>
2018-09-17 15:02:19 +0300

    add print appId - url to use is localhost:8080/payment-service

--- src/main/java/com/example/checkpoint/cloud/payment/service/paymentservice/controller.java
+++ src/main/java/com/example/checkpoint/cloud/payment/service/paymentservice/controller.java
@@ -1,5 +1,6 @@
 package com.example.checkpoint.cloud.payment.service.paymentservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class controller {
 
+	@Value("${eureka.instance.instanceId}")
+	private String appId;
+	
 	@RequestMapping
 	public @ResponseBody String getMsg()
 	{
-		return "This is payment-service.";
+		return "This is payment-service. "+appId;
 	}
 	
 }
